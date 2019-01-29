6cc5402d1f8e383a76f5386d7ba4f46e473e668b
Emily <847432003@qq.com>
2019-01-29 15:34:49 +0800

    添加Attr

---
+++ Edna.Extension/Express/AttributeExpress.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text; 
+
+#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+* 项目名称 ：Edna.Extension.Express
+* 项目描述 ：
+* 类 描 述 ：
+* 命名空间 ：Edna.Extension.Express
+* 机器名称 ：EMILY 
+* CLR 版本 ：4.0.30319.42000
+* 作    者 ：Emily
+* 创建时间 ：2019/1/23 13:39:51
+*******************************************************************
+* Copyright @ Emily 2019. All rights reserved.
+*******************************************************************
+//----------------------------------------------------------------*/
+#endregion
+namespace Edna.Extension.Express
+{
+    public class AttributeExpress
+    {
+        /// <summary>
+        /// 获取特性
+        /// </summary>
+        /// <typeparam name="T">Attribute</typeparam>
+        /// <typeparam name="K">Model</typeparam>
+        /// <param name="Express">表达式</param>
+        /// <returns></returns>
+        public static T GetAttributeType<T, K>(Expression<Func<K, Object>> Express)
+        {
+            if (Express == null) return default(T);
+            MemberExpression Exp = (MemberExpression)Express.Body;
+            var Attribute = (T)Exp.Member.GetCustomAttributes(typeof(T), true).FirstOrDefault();
+            return Attribute;
+        }
+    }
+}
---
+++ Edna.Extension/Express/PropertyExpress.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+#region << 版 本 注 释 >>
+/*----------------------------------------------------------------
+* 项目名称 ：Edna.Extension.Express
+* 项目描述 ：
+* 类 描 述 ：
+* 命名空间 ：Edna.Extension.Express
+* 机器名称 ：EMILY 
+* CLR 版本 ：4.0.30319.42000
+* 作    者 ：Emily
+* 创建时间 ：2019/1/23 14:23:02
+*******************************************************************
+* Copyright @ Emily 2019. All rights reserved.
+*******************************************************************
+//----------------------------------------------------------------*/
+#endregion
+namespace Edna.Extension.Express
+{
+    public class PropertyExpress
+    {
+        /// <summary>
+        /// 获取属性的名称和值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Param"></param>
+        /// <returns></returns>
+        public static IDictionary<String, Object> GetPropertiesValue<T>(T Param) where T : class, new()
+        {
+            ParameterExpression Parameter = Expression.Parameter(Param.GetType(), "t");
+            Dictionary<String, Object> Map = new Dictionary<String, Object>();
+            Param.GetType().GetProperties().ToList().ForEach(item =>
+            {
+                MemberExpression PropertyExpress = Expression.Property(Parameter, item);
+                UnaryExpression ConvterExpress = Expression.Convert(PropertyExpress, typeof(object));
+                Func<T, Object> GetValueFunc = Expression.Lambda<Func<T, object>>(ConvterExpress, Parameter).Compile();
+                Map.Add(item.Name, GetValueFunc(Param));
+            });
+            return Map;
+        }
+        /// <summary>
+        /// 指定属性赋值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="PropertyName"></param>
+        /// <returns></returns>
+        public static Action<T,Object> SetProptertyValue<T>(String PropertyName) where T : class, new()
+        {
+            var type = typeof(T);
+            var property = type.GetProperty(PropertyName);
+
+            var objectParameterExpression = Expression.Parameter(typeof(object), "obj");
+            var objectUnaryExpression = Expression.Convert(objectParameterExpression, type);
+
+            var valueParameterExpression = Expression.Parameter(typeof(object), "val");
+            var valueUnaryExpression = Expression.Convert(valueParameterExpression, property.PropertyType);
+
+            //// 调用给属性赋值的方法
+            var body = Expression.Call(objectUnaryExpression, property.GetSetMethod(), valueUnaryExpression);
+            var expression = Expression.Lambda<Action<T, object>>(body, objectParameterExpression, valueParameterExpression);
+
+            return expression.Compile();
+        }
+    }
+}
